ae1d31380dd8e4642586b117a83ce56174c746bd
zeeshan-za-ahmad <zeeshan-za-ahmad@gmail.com>
2019-03-22 22:53:52 +0500

    initial commit

--- public/assets/appConfig.sample.js
+++ public/assets/appConfig.sample.js
@@ -1,11 +1,11 @@
 // set your account sid here
-var accountSid = "AC...";
+var accountSid = "ACd2271ea9c3c8bcc791ef7d94bd873c23";
 
 
 var appConfig = {
   // Uncomment the following lines to enable automatic log in
   //
   // sso: {
-  //   accountSid: accountSid
+  //   accountSid: ACd2271ea9c3c8bcc791ef7d94bd873c23
   // }
 }
